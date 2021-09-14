6150a2b4f10c72882c913aa5fd72e92d41162659
Simon Pallister <sion.pallister@bigcommerce.com>
2021-09-14 23:23:58 +1000

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/client"
-import ProductListing from "../components/productlisting"
+import ProductListing from "../components/listing"
 
 export default function Index({products}) {
   const [ session, loading ] = useSession()
